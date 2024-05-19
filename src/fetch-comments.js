b359616ffde4dd7d97966054d4793bcd2ad0fbe2
milldr <miller0daniel@gmail.com>
2024-05-19 13:22:05 -0700

    Add step to fetch open PR comments before processing

---
+++ src/fetch-comments.js
@@ -0,0 +1,19 @@
+const { Octokit } = require("@octokit/action")
+
+const fetchPRComments = async function(octokit, owner, repo) {
+
+  const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues/comments', {
+    owner,
+    repo,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  console.log("PR Comments: %s", data)
+}
+
+const octokit = new Octokit()
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
+
+fetchPRComments(octokit, owner, repo, branch)
