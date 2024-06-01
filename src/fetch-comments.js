4f63305cb6163873385e87e7f89b54dca42abde4
milldr <miller0daniel@gmail.com>
2024-05-31 17:27:32 -0700

    Refactor fetchPRComments to use octokit.request for comments

--- src/fetch-comments.js
+++ src/fetch-comments.js
@@ -1,19 +1,14 @@
 import { Octokit } from "@octokit/action";
 
-const fetchPRComments = async function(octokit, owner, repo) {
-
-  const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues/comments', {
-    owner,
-    repo,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-
-  console.log("PR Comments: %s", data)
-}
-
 const octokit = new Octokit()
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
 
-fetchPRComments(octokit, owner, repo, branch)
+const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues/comments', {
+  owner,
+  repo,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+
+console.log("PR Comments: %s", data)
